735b5d089286a0a421142bc2e88c701c60367720
henrychow <113641610+henrow5@users.noreply.github.com>
2022-11-22 22:20:53 +0000

    Add music library album exercise

---
+++ 5_test_driving_find_method/music_library/app.rb
@@ -0,0 +1,40 @@
+require_relative "lib/database_connection"
+require_relative "lib/artist_repository"
+require_relative "lib/album_repository"
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect("music_library")
+
+# Perform a SQL query on the database and get the result set.
+# sql = "SELECT id, title FROM albums;"
+# result = DatabaseConnection.exec_params(sql, [])
+
+# Print out each record from the result set .
+# result.each do |record|
+#   p record
+# end
+
+# result = DatabaseConnection.exec_params("SELECT * FROM artists;", [])
+
+# puts result
+# result.each do |record|
+#   p record
+# end
+
+artist_repository = ArtistRepository.new
+album_repository = AlbumRepository.new
+
+# artist_repository.all.each do |artist|
+#   p artist
+# end
+
+# album_repository.all.each do |album|
+#   p album
+# end
+
+# artist = artist_repository.find(4)
+# puts artist.name
+
+# Get the album with id 3
+album = album_repository.find(3)
+
+puts "#{album.id} - #{album.title} - #{album.release_year}"
---
+++ 5_test_driving_find_method/music_library/spec/album_repository_spec.rb
@@ -0,0 +1,54 @@
+require "album_repository"
+
+describe AlbumRepository do
+  def reset_albums_table
+    seed_sql = File.read("spec/seeds_albums.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+    connection.exec(seed_sql)
+  end
+
+  def empty_albums_table
+    seed_sql = File.read("spec/seeds_empty.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_albums_table
+  end
+
+  it "returns two albums" do
+    repo = AlbumRepository.new
+    albums = repo.all
+
+    expect(albums.length).to eq(2)
+    expect(albums.first.title).to eq("Bossanova")
+    expect(albums.first.release_year).to eq("1999")
+    expect(albums.first.artist_id).to eq("1")
+  end
+
+  it "returns an empty array" do
+    empty_albums_table
+    repo = AlbumRepository.new
+    albums = repo.all
+    expect(albums).to eq([])
+  end
+
+  it "returns the single album Bossanova" do
+    repo = AlbumRepository.new
+    album = repo.find(1)
+
+    expect(album.title).to eq("Bossanova")
+    expect(album.release_year).to eq("1999")
+    expect(album.artist_id).to eq("1")
+  end
+
+  it "returns the single album Surfer Rosa" do
+    repo = AlbumRepository.new
+    album = repo.find(2)
+
+    expect(album.title).to eq("Surfer Rosa")
+    expect(album.release_year).to eq("2001")
+    expect(album.artist_id).to eq("1")
+  end
+end
