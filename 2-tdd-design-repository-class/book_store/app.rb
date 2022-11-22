39a0787f5efe322d0208cceb75b907c3896862a3
henrychow <113641610+henrow5@users.noreply.github.com>
2022-11-22 13:32:54 +0000

    Add book store challenge

---
+++ 2-tdd-design-repository-class/book_store/app.rb
@@ -0,0 +1,13 @@
+# file: app.rb
+require_relative "lib/database_connection"
+require_relative "lib/book_repository"
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect("book_store")
+
+# Perform a SQL query on the database and get the result set.
+book_repository = BookRepository.new
+
+book_repository.all.each do |book|
+  p "#{book.id} - #{book.title} - #{book.author_name}"
+end
